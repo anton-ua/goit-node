3ec61722444caaa218014241702f0c89e99b7a75
anton-ua <antonyua23@gmail.com>
2020-07-25 13:34:31 +0300

    hw-05

--- api/contacts/contact.model.js
+++ api/contacts/contact.model.js
@@ -6,6 +6,7 @@ const contactSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
+
   subscription: { type: String, required: true },
 });
 
